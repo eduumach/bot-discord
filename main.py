757e63558810040f5b0b44159a862d40b823dce4
Eduardo M. Rezende <eduardomachadorezende0@gmail.com>
2023-10-05 09:46:14 -0300

    Remove flask

--- main.py
+++ main.py
@@ -1,18 +1,5 @@
-from flask import Flask
-import threading
 import bot.main as bot  # Importe o módulo do seu bot
 
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "Bem-vindo à minha aplicação web!"
-
-def run_bot():
-    bot.run()
 
 if __name__ == '__main__':
-    bot_thread = threading.Thread(target=run_bot)
-    bot_thread.start()
-
-    app.run(host='0.0.0.0', port=8080)
+    bot.run()
